02e9206f78916269cb24cb42d9bad00de202366b
Abe Park <abepark01@gmail.com>
2016-09-03 13:02:14 -0700

    scripts for updating ubuntu and YCM

---
+++ update_ubuntu.sh
@@ -0,0 +1,4 @@
+sudo apt-get update -qq
+sudo apt-get upgrade -y
+sudo apt-get autoclean
+sudo apt-get autoremove -y
---
+++ update_ycm.sh
@@ -0,0 +1,2 @@
+CURRENT_DIR="$(cd "$(dirname "$0")" && pwd)"
+$CURRENT_DIR/bundle/YouCompleteMe/install.py --tern-completer --gocode-completer --clang-completer
