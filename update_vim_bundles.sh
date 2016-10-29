f29c00fd462876b7b812431b72d96a8d7b878183
Abe Park <abepark01@gmail.com>
2016-10-29 12:01:17 -0700

    update git submodule update script

--- update_vim_bundles.sh
+++ update_vim_bundles.sh
@@ -2,7 +2,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CURRENT_DIR=$PWD
 
 cd $DIR
-git submodule foreach git fetch
 git submodule foreach git pull origin master
+git submodule update --remote --merge
 git submodule update --init --recursive
 cd $CURRENT_DIR
