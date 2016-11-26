1f089f455a4c732888c894537a6b33f6b0ca0304
Abe Park <abepark01@gmail.com>
2016-11-26 15:45:14 -0800

    phpcomplete.vim added

--- setup_vim.sh
+++ setup_vim.sh
@@ -6,6 +6,11 @@ then
   ln -s $DIR/vimrc $HOME/.vimrc
 fi
 
+if [ ! -L $HOME/.vimrc.local ]
+then
+  ln -s $DIR/vimrc.local $HOME/.vimrc.local
+fi
+
 mkdir -p $HOME/.vim/autoload $HOME/.vim/colors
 curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
@@ -15,6 +20,11 @@ cp -r $DIR/bundle/vim-tomorrow-theme/colors $HOME/.vim
 if [ ! -L $HOME/.vim/bundle ]
 then
   ln -s $DIR/bundle $HOME/.vim/bundle
+fi
+
+if [ ! -L $HOME/.vim/ftplugin ]
+then
+  ln -s $DIR/vim/ftplugin $HOME/.vim/ftplugin
 fi
 
 cd $CURRENT_DIR
