1a1ad58e2381b3dc2556a38a670f22996b89e700
Abe Park <abe.park@riotgames.com>
2016-12-14 18:16:10 -0800

    compile YCM with rust lang support

--- update_ycm.sh
+++ update_ycm.sh
@@ -1,2 +1,2 @@
 CURRENT_DIR="$(cd "$(dirname "$0")" && pwd)"
-$CURRENT_DIR/bundle/YouCompleteMe/install.py --tern-completer --gocode-completer --clang-completer
+$CURRENT_DIR/vim/bundle/YouCompleteMe/install.py --tern-completer --gocode-completer --clang-completer --racer-completer
