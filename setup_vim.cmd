86e62f5a59183446dc7ed0fee11b0b9cc86bd097
Abe Park <abepark01@gmail.com>
2017-04-11 22:15:12 -0700

    fixed win setup script for vim

--- setup_vim.cmd
+++ setup_vim.cmd
@@ -4,4 +4,4 @@ SET VIM_DIR=%USERPROFILE%/vimfiles
 rem "Copy vimrc"
 copy vimrc %USERPROFILE%\_vimrc
 rem "Copy vimrc.bundles"
-copy vimrc.bundles %VIM_DIR%
+copy vimrc.bundles "%VIM_DIR%/vimrc.bundles"
