0a7d3a90cfa93996967278d96839aedb94b32829
Abe Park <abepark01@gmail.com>
2017-08-02 08:16:39 -0700

    base16 setup script

--- setup_base16_bashrc.sh
+++ setup_base16_bashrc.sh
@@ -1,7 +1,8 @@
-if [ ! -d ~/.config/base16-shell ]; then
+if [ ! -d ~/.config/base16-shell ]
+then
   git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
-  if [ -e $HOME/.bashrc]
+  if [ -e $HOME/.bashrc ]
   then
     echo -e 'BASE16_SHELL=$HOME/.config/base16-shell/' >> ~/.bashrc
     echo -e '[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"' >> ~/.bashrc
