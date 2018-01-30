6c15214303e74d50fe795ddb820a7f4f90ec3682
Abe Park <abepark01@gmail.com>
2018-01-30 13:00:07 -0800

    change order of bash config scripts to check for

--- setup_base16_bashrc.sh
+++ setup_base16_bashrc.sh
@@ -1,14 +1,14 @@
 rm -fr ~/.config/base16-shell
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
-if [ -e $HOME/.bashrc ]
-then
-  echo -e 'BASE16_SHELL=$HOME/.config/base16-shell/' >> ~/.bashrc
-  echo -e '[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"' >> ~/.bashrc
-  echo -e 'base16_oceanicnext' >> ~/.bashrc
-elif [ -e $HOME/.bash_profile ]
+if [ -e $HOME/.bash_profile ]
 then
   echo -e 'BASE16_SHELL=$HOME/.config/base16-shell/' >> ~/.bash_profile
   echo -e '[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"' >> ~/.bash_profile
   echo -e 'base16_oceanicnext' >> ~/.bash_profile
+elif [ -e $HOME/.bashrc ]
+then
+  echo -e 'BASE16_SHELL=$HOME/.config/base16-shell/' >> ~/.bashrc
+  echo -e '[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"' >> ~/.bashrc
+  echo -e 'base16_oceanicnext' >> ~/.bashrc
 fi
