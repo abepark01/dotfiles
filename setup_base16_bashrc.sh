decb33586aab6e0a36c6a5813006a2f442b415ba
Abe Park <abepark01@gmail.com>
2018-02-06 09:55:59 -0800

    fix base16 shell setup

--- setup_base16_bashrc.sh
+++ setup_base16_bashrc.sh
@@ -3,12 +3,12 @@ git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shel
 
 if [ -e $HOME/.bash_profile ]
 then
-  echo -e 'BASE16_SHELL=$HOME/.config/base16-shell/' >> ~/.bash_profile
-  echo -e '[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"' >> ~/.bash_profile
-  echo -e 'base16_oceanicnext' >> ~/.bash_profile
+  echo 'BASE16_SHELL=$HOME/.config/base16-shell/' >> ~/.bash_profile
+  echo  '[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"' >> ~/.bash_profile
+  echo  'base16_oceanicnext' >> ~/.bash_profile
 elif [ -e $HOME/.bashrc ]
 then
-  echo -e 'BASE16_SHELL=$HOME/.config/base16-shell/' >> ~/.bashrc
-  echo -e '[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"' >> ~/.bashrc
-  echo -e 'base16_oceanicnext' >> ~/.bashrc
+  echo  'BASE16_SHELL=$HOME/.config/base16-shell/' >> ~/.bashrc
+  echo  '[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"' >> ~/.bashrc
+  echo  'base16_oceanicnext' >> ~/.bashrc
 fi
