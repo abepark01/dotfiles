6d507d90414d482f8b7c95ee44dd2ea93ca60dce
Abe Park <abepark01@gmail.com>
2018-02-06 17:56:18 -0500

    fix compile script for neovim

--- scripts/build-nvim.sh
+++ scripts/build-nvim.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PREFIX=$HOME/.local
 PYTHON3_CONFIG_DIR="$(python3.5m-config --configdir)"
-cd $HOME/scripts/neovim
-make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.local" CMAKE_BUILD_TYPE=Release
+cd ~/src/neovim
+make clean
+make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/.local -DCMAKE_BUILD_TYPE=Release"
 make install
