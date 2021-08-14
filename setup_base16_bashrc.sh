5a3710394dfe77d030e80d2a1c9396a4868cb138
Abe Park <{ID}+{username}@users.noreply.github.com>
2021-08-14 14:18:24 -0700

    fix order

--- setup_base16_bashrc.sh
+++ setup_base16_bashrc.sh
@@ -7,14 +7,14 @@ then
   echo 'BASE16_SHELL=$HOME/.config/base16-shell/' >> ~/.bash_profile
   echo  '[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"' >> ~/.bash_profile
   echo  $THEME >> ~/.bash_profile
-elif [ -e $HOME/.bashrc ]
-then
-  echo  'BASE16_SHELL=$HOME/.config/base16-shell/' >> ~/.bashrc
-  echo  '[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"' >> ~/.bashrc
-  echo  $THEME >> ~/.bashrc
 elif [ -e $HOME/.zshrc ]
 then
   echo  'BASE16_SHELL=$HOME/.config/base16-shell/' >> ~/.zshrc
   echo  '[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"' >> ~/.zshrc
   echo  $THEME >> ~/.zshrc
+elif [ -e $HOME/.bashrc ]
+then
+  echo  'BASE16_SHELL=$HOME/.config/base16-shell/' >> ~/.bashrc
+  echo  '[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"' >> ~/.bashrc
+  echo  $THEME >> ~/.bashrc
 fi
