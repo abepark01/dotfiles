1c96ffd17bfeda916e14ff47c85985c6e56da6b0
Abe <7087312+abepark01@users.noreply.github.com>
2022-09-07 03:00:25 -0700

    add lsp for Javascript

--- config/nvim/plugin/lspconfig.rc.lua
+++ config/nvim/plugin/lspconfig.rc.lua
@@ -56,16 +56,12 @@ end
 local capabilities = require('cmp_nvim_lsp').update_capabilities(
   vim.lsp.protocol.make_client_capabilities()
 )
-nvim_lsp.flow.setup {
-  on_attach = on_attach,
-  capabilities = capabilities
-}
 
 nvim_lsp.tailwindcss.setup {}
 
 nvim_lsp.tsserver.setup {
   on_attach = on_attach,
-  filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "typescript.tsx" },
   cmd = { "typescript-language-server", "--stdio" },
   capabilities = capabilities,
 }
