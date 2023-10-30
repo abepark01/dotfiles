3679387bf5e3d338b743bd6da13f92396bfa869c
Abe <7087312+abepark01@users.noreply.github.com>
2023-10-30 00:15:43 -0700

    migrate to lazy.nvim

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -1,52 +1,67 @@
-local status, packer = pcall(require, "packer")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local status, lazy = pcall(require, "lazy")
 
 if (not status) then
-  print("Packer is not installed")
+  print("lazy is not installed")
   return
 end
 
-vim.cmd [[packadd packer.nvim]]
-
-packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'tyrannicaltoucan/vim-quantum'
-  use {
+lazy.setup({
+  'tyrannicaltoucan/vim-quantum',
+  {
     'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-  }
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/nvim-cmp'
-  use 'onsails/lspkind.nvim'
-  use 'L3MON4D3/LuaSnip'
-  use {
+    dependencies = {{ 'kyazdani42/nvim-web-devicons', lazy = true }}
+  },
+  'neovim/nvim-lspconfig',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/nvim-cmp',
+  'onsails/lspkind.nvim',
+  'L3MON4D3/LuaSnip',
+  {
     'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
-  use 'windwp/nvim-autopairs'
-  use 'windwp/nvim-ts-autotag'
-  use 'nvim-lua/plenary.nvim'
-  use 'folke/trouble.nvim'
-  use 'nvim-telescope/telescope.nvim'
-  use 'nvim-telescope/telescope-fzf-native.nvim'
-
-  use 'nvim-telescope/telescope-file-browser.nvim'
-  use 'norcalli/nvim-colorizer.lua'
-  use { 'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons' }
-  use 'nvimdev/lspsaga.nvim'
-  use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
-  use 'jay-babu/mason-null-ls.nvim'
-  use 'MunifTanjim/prettier.nvim' -- Prettier plugin for Neovim's built-in LSP client
-  use { "williamboman/mason.nvim" }
-  use 'williamboman/mason-lspconfig.nvim'
-  use 'kyazdani42/nvim-web-devicons'
-  use 'tpope/vim-rails'
-  use 'tpope/vim-endwise'
-  use 'tpope/vim-repeat'
-  use 'tpope/vim-surround'
-  use 'tpope/vim-bundler'
-  use 'editorconfig/editorconfig-vim'
-  use 'prettier/vim-prettier'
-  use 'kchmck/vim-coffee-script'
-end)
+    build = ':TSUpdate'
+  },
+  {
+    'nvimdev/lspsaga.nvim',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter', -- optional
+      'nvim-tree/nvim-web-devicons'     -- optional
+    }
+  },
+  'windwp/nvim-autopairs',
+  'windwp/nvim-ts-autotag',
+  'nvim-lua/plenary.nvim',
+  'folke/trouble.nvim',
+  'nvim-telescope/telescope.nvim',
+  'nvim-telescope/telescope-fzf-native.nvim',
+  'nvim-telescope/telescope-file-browser.nvim',
+  'norcalli/nvim-colorizer.lua',
+  { 'akinsho/bufferline.nvim', version = "v2.*", dependencies = {'kyazdani42/nvim-web-devicons' }},
+  'jose-elias-alvarez/null-ls.nvim', -- Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+  'jay-babu/mason-null-ls.nvim',
+  'MunifTanjim/prettier.nvim', -- Prettier plugin for Neovim's built-in LSP client
+  "williamboman/mason.nvim",
+  'williamboman/mason-lspconfig.nvim',
+  'kyazdani42/nvim-web-devicons',
+  'tpope/vim-rails',
+  'tpope/vim-endwise',
+  'tpope/vim-repeat',
+  'tpope/vim-surround',
+  'tpope/vim-bundler',
+  'editorconfig/editorconfig-vim',
+  'prettier/vim-prettier',
+  'kchmck/vim-coffee-script',
+})
